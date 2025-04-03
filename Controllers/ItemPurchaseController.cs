af173ec984b8ec8420fbd6be361d4892f6ccdf1e
Markus Lima <Markusmak@gmail.com>
2025-04-03 17:02:35 -0300

    correção na rota de atualizar item

---
+++ Controllers/ItemPurchaseController.cs
@@ -0,0 +1,45 @@
+﻿using BookStoreAPI.Interface;
+using BookStoreAPI.Models.DTOs.ItenOfPurchase;
+using BookStoreAPI.Tools;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BookStoreAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ItemPurchaseController : Controller
+    {
+        private readonly IPurchaseSevice _purchaseService;
+
+        public ItemPurchaseController(IPurchaseSevice purchaseService)
+        {
+            _purchaseService = purchaseService;
+        }
+
+
+        [HttpPost]
+        //[RoleMiddleware("admin")]
+        public async Task<ActionResult> UpdateItemPurchaseAsync(ReadItenOfPurchaseDTO itenOfPurchase)
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+
+            try
+            {
+                var result = await _purchaseService.UpdateItemPurchaseAsync(itenOfPurchase);
+                return NoContent();
+            }
+            catch (ExceptionsCode ex)
+            {
+                return StatusCode(ex.StatusCode, ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
+        }
+
+
+    }
+}
--- Controllers/PurchasesController.cs
+++ Controllers/PurchasesController.cs
@@ -17,16 +17,13 @@ namespace BookStoreAPI.Controllers
             _purchaseService = purchaseService;
         }
 
-        public async Task<ActionResult<IEnumerable<ReadPurchaseDTO>>> GetBooks([FromQuery] int skip = 0, [FromQuery] int take = 10)
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ReadPurchaseDTO>>> GetPurchasesAsync([FromQuery] int skip = 0, [FromQuery] int take = 10)
         {
             try
             {
                 var result = await _purchaseService.GetPurchasesAsync(skip, take);
                 return Ok(result);
-            }
-            catch (ExceptionsCode ex)
-            {
-                return StatusCode(ex.StatusCode, ex.Message);
             }
             catch (Exception ex)
             {
@@ -104,27 +101,6 @@ namespace BookStoreAPI.Controllers
             try
             {
                 var result = await _purchaseService.DeletePurchaseAsync(id);
-                return NoContent();
-            }
-            catch (ExceptionsCode ex)
-            {
-                return StatusCode(ex.StatusCode, ex.Message);
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(500, $"Internal server error: {ex.Message}");
-            }
-        }
-
-        [HttpPost]
-        //[RoleMiddleware("admin")]
-        public async Task<ActionResult> UpdateItemPurchaseAsync(ReadItenOfPurchaseDTO itenOfPurchase)
-        {
-            if (!ModelState.IsValid) return BadRequest(ModelState);
-
-            try
-            {
-                var result = await _purchaseService.UpdateItemPurchaseAsync(itenOfPurchase);
                 return NoContent();
             }
             catch (ExceptionsCode ex)
